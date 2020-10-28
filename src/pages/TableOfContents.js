354239127a6489665fdf18f4c49670f9168bee24
Anitesh Lal <anitesh93@gmail.com>
2020-10-27 17:24:53 -0700

    created sample pages.

---
+++ src/pages/TableOfContents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TableOfContents() {
+  return (
+    <div>
+      <h1>Table of contents</h1>
+    </div>
+  );
+}
+
+export default TableOfContents;
---
+++ src/pages/WhatIsReact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WhatIsReact() {
+  return (
+    <div>
+      <h1>What is React?</h1>
+    </div>
+  );
+}
+
+export default WhatIsReact;
