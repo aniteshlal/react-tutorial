eb8f3468270900489ac378755eb0cfb543bfe4c5
Anitesh Lal <anitesh93@gmail.com>
2020-11-02 00:16:47 -0800

    Displayed based on weather there is a previous or next. So the first page wont have a previous button and the last page wont have a next button

--- src/components/Pagnation.js
+++ src/components/Pagnation.js
@@ -5,17 +5,27 @@ import { arrowBack, arrowForward } from 'ionicons/icons';
 
 function Pagnation({ prev, next }) {
   return (
-    <div className='text-center'>
-      <IonButton routerLink={prev}>
-        <IonIcon slot='start' icon={arrowBack} />
-        Previous
-      </IonButton>
-
-      <IonButton routerLink={next}>
-        Next
-        <IonIcon slot='end' icon={arrowForward} />
-      </IonButton>
-    </div>
+    <>
+      <hr />
+      <div className='text-center'>
+        {prev ? (
+          <IonButton routerLink={prev}>
+            <IonIcon slot='start' icon={arrowBack} />
+            Previous
+          </IonButton>
+        ) : (
+          ''
+        )}
+        {next ? (
+          <IonButton routerLink={next}>
+            Next
+            <IonIcon slot='end' icon={arrowForward} />
+          </IonButton>
+        ) : (
+          ''
+        )}
+      </div>
+    </>
   );
 }
 
